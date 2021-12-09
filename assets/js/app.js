2febee0a84be252ee3d1cbed9d94ea32988c59bd
Anuchart Srikongrak <my.anuchart.srikongrak@gmail.com>
2021-12-09 23:01:12 +0700

    remove jquery code, add pure js scrollbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,116 +1,214 @@
 "use strict";
 
-var _AppTheme = (function () {
-    var appThemeColors = ["default"];
+var _toConsumableArray = function (arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } };
+
+var _HelperSelectorEngine = (function () {
     return {
-        init: function init(colors) {
+        find: function find(selector) {
+            var _ref;
+
+            var element = arguments[1] === undefined ? document.documentElement : arguments[1];
+
+            return (_ref = []).concat.apply(_ref, _toConsumableArray(Element.prototype.querySelectorAll.call(element, selector)));
+        },
+        findOne: function findOne(selector) {
+            var element = arguments[1] === undefined ? document.documentElement : arguments[1];
+
+            return Element.prototype.querySelector.call(element, selector);
+        },
+        children: function children(element, selector) {
+            var _ref;
+
+            return (_ref = []).concat.apply(_ref, _toConsumableArray(element.children)).filter(function (child) {
+                return child.matches(selector);
+            });
+        },
+        parents: (function (_parents) {
+            var _parentsWrapper = function parents(_x, _x2) {
+                return _parents.apply(this, arguments);
+            };
+
+            _parentsWrapper.toString = function () {
+                return _parents.toString();
+            };
+
+            return _parentsWrapper;
+        })(function (element, selector) {
+            var parents = [];
+            var ancestor = element.parentNode;
+            while (ancestor && ancestor.nodeType === Node.ELEMENT_NODE && ancestor.nodeType !== NODE_TEXT) {
+                if (ancestor.matches(selector)) {
+                    parents.push(ancestor);
+                }
+                ancestor = ancestor.parentNode;
+            }
+            return parents;
+        }),
+        prev: function prev(element, selector) {
+            var previous = element.previousElementSibling;
+            while (previous) {
+                if (previous.matches(selector)) {
+                    return [previous];
+                }
+                previous = previous.previousElementSibling;
+            }
+            return [];
+        },
+        next: (function (_next) {
+            var _nextWrapper = function next(_x, _x2) {
+                return _next.apply(this, arguments);
+            };
+
+            _nextWrapper.toString = function () {
+                return _next.toString();
+            };
+
+            return _nextWrapper;
+        })(function (element, selector) {
+            var next = element.nextElementSibling;
+            while (next) {
+                if (next.matches(selector)) {
+                    return [next];
+                }
+                next = next.nextElementSibling;
+            }
+            return [];
+        })
+    };
+})();
+
+var _AppHelpers = (function () {
+    return {
+        SelectorEngine: _HelperSelectorEngine
+    };
+})();
+
+var _AppTheme = (function () {
+    var appThemeColors = [];
+    return {
+        init: function init() {
+            var colors = arguments[0] === undefined ? [] : arguments[0];
+
             appThemeColors = colors;
             var currentThemeColor = localStorage.getItem("app-theme-color");
             var currentThemeDark = localStorage.getItem("app-theme-dark");
-            if (!currentThemeColor) this.changeTheme("default", currentThemeDark === "dark");else this.changeTheme(currentThemeColor, currentThemeDark === "dark");
+            this.changeTheme(currentThemeColor ? currentThemeColor : "default", currentThemeDark ? currentThemeDark : "");
             return this;
         },
         toggleDarkMode: function toggleDarkMode() {
-            $("html").toggleClass("dark");
-            this.setThemeDark($("html").hasClass("dark") ? "dark" : "");
+            document.documentElement.classList.toggle("dark");
+            this.setThemeDark(document.documentElement.classList.contains("dark") ? "dark" : "");
         },
         changeTheme: function changeTheme() {
             var theme = arguments[0] === undefined ? "default" : arguments[0];
-            var dark = arguments[1] === undefined ? false : arguments[1];
+            var dark = arguments[1] === undefined ? "" : arguments[1];
 
-            if ($("html").hasClass("dark")) dark = true;
-            this.setThemeDark(dark ? "dark" : "");
-
+            if (document.documentElement.classList.contains("dark")) dark = "dark";
+            this.setThemeDark(dark);
             if (!appThemeColors.includes(theme)) theme = "default";
             this.setThemeColor(theme);
-
-            $("html").removeClass();
-            $("html").addClass("theme--" + theme + " " + (dark ? "dark" : ""));
+            document.documentElement.className = "theme--" + theme + " " + dark;
         },
         setThemeColor: function setThemeColor(theme) {
             localStorage.setItem("app-theme-color", theme);
         },
         setThemeDark: function setThemeDark(dark) {
             localStorage.setItem("app-theme-dark", dark);
-        }
-    };
+        } };
 })();
 
-var _handleVendor_jqueryScrollbar = function _handleVendor_jqueryScrollbar() {
-    $("[data-vendor=\"jquery.scrollbar\"]").addClass("scrollbar").addClass("scrollbar-macosx").scrollbar({
-        ignoreOverlay: true,
-        ignoreMobile: true,
-        disableBodyScroll: false
+var _handleVendor_scrollbot = function _handleVendor_scrollbot() {
+    new Scrollbot("[data-vendor=\"scrollbot\"]");
+};
+
+var _handle_sidebar_state = function _handle_sidebar_state() {
+    var currentPaths = window.location.pathname.split("/");
+    var currentPath = "" + currentPaths[currentPaths.length - 1];
+
+    var sidebar = _AppHelpers.SelectorEngine.findOne(".nav-sidebar");
+
+    sidebar.classList.add("show-children");
+
+    // main sidebar
+    var mainSidebarItems = _AppHelpers.SelectorEngine.find(".sidebar-main-item");
+    mainSidebarItems.forEach(function (mainItem, mainItemIndex) {
+        var button = _AppHelpers.SelectorEngine.findOne("a.btn", mainItem);
+        if (button && button.getAttribute("href") === currentPath) {
+            mainItem.classList.add("active");
+            sidebar.classList.remove("show-children");
+            return;
+        }
+
+        // children sidebar
+        var childrenSidebarItems = _AppHelpers.SelectorEngine.find(".sidebar-children-item", mainItem);
+        childrenSidebarItems.forEach(function (childrenItem, childrenItemIndex) {
+            var button = _AppHelpers.SelectorEngine.findOne("a", childrenItem);
+            if (button && button.getAttribute("href") === currentPath) {
+                childrenItem.classList.add("active");
+                mainItem.classList.add("active");
+                if (_AppHelpers.SelectorEngine.findOne("ul.nav-sidebar-children", mainItem)) {
+                    sidebar.classList.add("show-children");
+                }
+            }
+        });
+    });
+};
+
+var _handle_sidebar_actions = function _handle_sidebar_actions() {
+    var sidebar = _AppHelpers.SelectorEngine.findOne(".nav-sidebar");
+
+    var mainSidebarItems = _AppHelpers.SelectorEngine.find(".sidebar-main-item");
+    mainSidebarItems.forEach(function (mainItem, index) {
+        var button = _AppHelpers.SelectorEngine.findOne("a.btn", mainItem);
+        if (button) {
+            button.addEventListener("click", function () {
+                mainSidebarItems.forEach(function (mainItemTemp, indexTemp) {
+                    mainItemTemp.classList.remove("active");
+                    if (_AppHelpers.SelectorEngine.findOne("ul.nav-sidebar-children", mainItem)) {
+                        sidebar.classList.add("show-children");
+                    }
+                });
+                document.body.classList.add("show-sidebar");
+                mainItem.classList.add("active");
+            });
+        }
+    });
+
+    var toggleButtons = _AppHelpers.SelectorEngine.find("[data-click=\"sidebar-toggle\"]");
+    toggleButtons.forEach(function (button, index) {
+        button.addEventListener("click", function () {
+            document.body.classList.toggle("show-sidebar");
+        });
     });
 };
 
 var App = (function () {
     "use strict";
-    var appThemeColors = ["default", "green", "orange"];
-
-    var _appTheme = _AppTheme.init(appThemeColors);
-
+    var _appThemeColors = ["default", "green", "orange"];
+    var _appTheme = _AppTheme.init(_appThemeColors);
     return {
         init: function init() {
-            _handleVendor_jqueryScrollbar();
+            this.initSidebar();
+            this.initVendor();
         },
-        theme: _appTheme
-    };
+        initSidebar: function initSidebar() {
+            _handle_sidebar_actions();
+            _handle_sidebar_state();
+        },
+        initVendor: function initVendor() {
+            _handleVendor_scrollbot();
+        },
+        theme: _appTheme,
+        helpers: _AppHelpers };
 })();
 
-var _handleSidebar = function _handleSidebar() {
-    $("[data-click=\"sidebar-toggle\"]").on("click", function (e) {
-        $("body").toggleClass("show-sidebar");
-    });
-
-    var currentPaths = window.location.pathname.split("/");
-    var currentPath = "" + currentPaths[currentPaths.length - 1];
-    // console.log(window.location);
-
-    $(".nav-sidebar ul.nav-sidebar-main li > a.btn").each(function (index, element) {
-        if ("" + $(element).attr("href") === currentPath) {
-            $(element).parents(".nav-sidebar").removeClass("show-sub");
-            $(element).parent().addClass("active");
-        }
-    });
-    $(".nav-sidebar ul.nav-sidebar-main li > a:not(.btn)").each(function (index, element) {
-        if ($(element).attr("href") === currentPath) {
-            $(element).parent().addClass("active");
-            $(element).parents(".nav-sidebar").addClass("show-sub");
-            $(element).parents(".nav-sidebar-children").prev().parent().addClass("active");
-            $("body").addClass("show-sidebar");
-        }
-    });
-
-    $(".nav-sidebar ul.nav-sidebar-main li > button.btn").on("click", function (e) {
-        $(e.target).parents("ul.nav-sidebar-main").find("li.sidebar-main-item").removeClass("active");
-        $(e.target).parents(".nav-sidebar").find("li.sidebar-main-item").removeClass("show-sub");
-
-        $(e.target).parent().addClass("active");
-        $("body").removeClass("show-sidebar");
-        $(e.target).parents(".nav-sidebar").removeClass("show-sub");
-        if ($(e.target).next().hasClass("nav-sidebar-children")) {
-            $(e.target).parents(".nav-sidebar").addClass("show-sub");
-            $("body").addClass("show-sidebar");
-        }
-    });
-    if ($(".nav-sidebar-main").children("li.active").length) {
-        var sub = $(".nav-sidebar-main").children("li.active").find("ul.nav-sidebar-children");
-        if (sub.find("li.active").length) {
-            $(".nav-sidebar").addClass("show-sub");
-        }
-    }
-};
 var TestApp = (function () {
     "use strict";
     return {
-        init: function init() {
-            _handleSidebar();
-        }
-    };
+        init: function init() {} };
 })();
 
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
     App.init();
     TestApp.init();
-});
+}, false);
--- src/js/app.js
+++ src/js/app.js
@@ -1,114 +1,184 @@
-const _AppTheme = function() {
-    let appThemeColors = ['default'];
+const _HelperSelectorEngine = (function () {
     return {
-        init: function(colors) {
+        find: function (selector, element = document.documentElement) {
+            return [].concat(
+                ...Element.prototype.querySelectorAll.call(element, selector)
+            );
+        },
+        findOne: function (selector, element = document.documentElement) {
+            return Element.prototype.querySelector.call(element, selector);
+        },
+        children: function (element, selector) {
+            return []
+                .concat(...element.children)
+                .filter((child) => child.matches(selector));
+        },
+        parents: function (element, selector) {
+            const parents = [];
+            let ancestor = element.parentNode;
+            while (ancestor && ancestor.nodeType === Node.ELEMENT_NODE && ancestor.nodeType !== NODE_TEXT) {
+                if (ancestor.matches(selector)) {
+                    parents.push(ancestor);
+                }
+                ancestor = ancestor.parentNode;
+            }
+            return parents;
+        },
+        prev: function (element, selector) {
+            let previous = element.previousElementSibling;
+            while (previous) {
+                if (previous.matches(selector)) {
+                    return [previous];
+                }
+                previous = previous.previousElementSibling;
+            }
+            return [];
+        },
+        next: function (element, selector) {
+            let next = element.nextElementSibling;
+            while (next) {
+                if (next.matches(selector)) {
+                    return [next];
+                }
+                next = next.nextElementSibling;
+            }
+            return [];
+        }
+    };
+})();
+
+const _AppHelpers = (function () {
+    return {
+        SelectorEngine: _HelperSelectorEngine
+    };
+})();
+
+const _AppTheme = (function () {
+    let appThemeColors = [];
+    return {
+        init: function (colors = []) {
             appThemeColors = colors;
-            const currentThemeColor = localStorage.getItem('app-theme-color');
-            const currentThemeDark = localStorage.getItem('app-theme-dark');
-            if(!currentThemeColor) this.changeTheme('default', currentThemeDark === 'dark');
-            else this.changeTheme(currentThemeColor, currentThemeDark === 'dark');
+            const currentThemeColor = localStorage.getItem("app-theme-color");
+            const currentThemeDark = localStorage.getItem("app-theme-dark");
+            this.changeTheme(currentThemeColor ? currentThemeColor : "default", currentThemeDark ? currentThemeDark : "");
             return this;
         },
-        toggleDarkMode: function() {
-            $('html').toggleClass('dark');
-            this.setThemeDark($('html').hasClass('dark') ? 'dark' : '');
+        toggleDarkMode: function () {
+            document.documentElement.classList.toggle("dark");
+            this.setThemeDark(document.documentElement.classList.contains("dark") ? "dark" : "");
         },
-        changeTheme: function(theme = 'default', dark = false) {
-
-            if($('html').hasClass('dark')) dark = true;
-            this.setThemeDark(dark ? 'dark' : '');
-
-            if(!appThemeColors.includes(theme)) theme = 'default';
+        changeTheme: function (theme = "default", dark = "") {
+            if (document.documentElement.classList.contains("dark")) dark = "dark";
+            this.setThemeDark(dark);
+            if (!appThemeColors.includes(theme)) theme = "default";
             this.setThemeColor(theme);
-
-            $('html').removeClass();
-            $('html').addClass(`theme--${theme} ${dark ? 'dark' : ''}`);
+            document.documentElement.className = `theme--${theme} ${dark}`;
         },
-        setThemeColor(theme) {
-            localStorage.setItem('app-theme-color', theme);
+        setThemeColor: function (theme) {
+            localStorage.setItem("app-theme-color", theme);
         },
-        setThemeDark(dark) {
-            localStorage.setItem('app-theme-dark', dark);
-        }
+        setThemeDark: function (dark) {
+            localStorage.setItem("app-theme-dark", dark);
+        },
     };
-}();
+})();
 
-const _handleVendor_jqueryScrollbar = function() {
-    $(`[data-vendor="jquery.scrollbar"]`).addClass('scrollbar').addClass('scrollbar-macosx').scrollbar({
-        ignoreOverlay: true,
-        ignoreMobile: true,
-        disableBodyScroll : false
-    });
-}
+const _handleVendor_scrollbot = function () {
+    new Scrollbot(`[data-vendor="scrollbot"]`);
+};
 
-var App = function() {
-    "use strict";
-    let appThemeColors = ['default', 'green', 'orange'];
-
-    const _appTheme = _AppTheme.init(appThemeColors);
-    
-    return {
-        init: function() {
-            _handleVendor_jqueryScrollbar();
-        },
-        theme: _appTheme
-    };
-}();
-
-
-const _handleSidebar = function() {
-    $('[data-click="sidebar-toggle"]').on('click', (e) => {
-        $('body').toggleClass('show-sidebar');
-    });
-
-    const currentPaths = window.location.pathname.split('/');
+const _handle_sidebar_state = function() {
+    const currentPaths = window.location.pathname.split("/");
     const currentPath = `${currentPaths[currentPaths.length - 1]}`;
-    // console.log(window.location);
 
-    $('.nav-sidebar ul.nav-sidebar-main li > a.btn').each((index, element) => {
-        if(`${$(element).attr('href')}` === currentPath) {
-            $(element).parents('.nav-sidebar').removeClass('show-sub');
-            $(element).parent().addClass('active');
+    const sidebar = _AppHelpers.SelectorEngine.findOne(`.nav-sidebar`);
+
+    sidebar.classList.add('show-children');
+
+    // main sidebar
+    const mainSidebarItems = _AppHelpers.SelectorEngine.find(`.sidebar-main-item`);
+    mainSidebarItems.forEach((mainItem, mainItemIndex) => {
+        const button = _AppHelpers.SelectorEngine.findOne('a.btn', mainItem);
+        if(button && button.getAttribute('href') === currentPath) {
+            mainItem.classList.add('active');
+            sidebar.classList.remove('show-children');
+            return;
+        }
+
+        // children sidebar
+        const childrenSidebarItems = _AppHelpers.SelectorEngine.find(`.sidebar-children-item`, mainItem);
+        childrenSidebarItems.forEach((childrenItem, childrenItemIndex) => {
+            const button = _AppHelpers.SelectorEngine.findOne('a', childrenItem);
+            if(button && button.getAttribute('href') === currentPath) {
+                childrenItem.classList.add('active');
+                mainItem.classList.add('active');
+                if(_AppHelpers.SelectorEngine.findOne('ul.nav-sidebar-children', mainItem)) {
+                    sidebar.classList.add('show-children');
+                }
+            }
+        });
+    });
+};
+
+const _handle_sidebar_actions = function() {
+    const sidebar = _AppHelpers.SelectorEngine.findOne(`.nav-sidebar`);
+
+    const mainSidebarItems = _AppHelpers.SelectorEngine.find(`.sidebar-main-item`);
+    mainSidebarItems.forEach((mainItem, index) => {
+        const button = _AppHelpers.SelectorEngine.findOne('a.btn', mainItem);
+        if(button) {
+            button.addEventListener('click', () => {
+                mainSidebarItems.forEach((mainItemTemp, indexTemp) => {
+                    mainItemTemp.classList.remove('active');
+                    if(_AppHelpers.SelectorEngine.findOne('ul.nav-sidebar-children', mainItem)) {
+                        sidebar.classList.add('show-children');
+                    }
+                });
+                document.body.classList.add('show-sidebar');
+                mainItem.classList.add('active');
+            });
         }
     });
-    $('.nav-sidebar ul.nav-sidebar-main li > a:not(.btn)').each((index, element) => {
-        if($(element).attr('href') === currentPath) {
-            $(element).parent().addClass('active');
-            $(element).parents('.nav-sidebar').addClass('show-sub');
-            $(element).parents('.nav-sidebar-children').prev().parent().addClass('active');
-            $('body').addClass('show-sidebar');
-        }
-    });
 
-    $('.nav-sidebar ul.nav-sidebar-main li > button.btn').on('click', (e) => {
-        $(e.target).parents('ul.nav-sidebar-main').find('li.sidebar-main-item').removeClass('active');
-        $(e.target).parents('.nav-sidebar').find('li.sidebar-main-item').removeClass('show-sub');
-
-        $(e.target).parent().addClass('active');
-        $('body').removeClass('show-sidebar');
-        $(e.target).parents('.nav-sidebar').removeClass('show-sub');
-        if ($(e.target).next().hasClass('nav-sidebar-children')) {
-            $(e.target).parents('.nav-sidebar').addClass('show-sub');
-            $('body').addClass('show-sidebar');
-        }
+    const toggleButtons = _AppHelpers.SelectorEngine.find(`[data-click="sidebar-toggle"]`);
+    toggleButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            document.body.classList.toggle('show-sidebar');
+        });
     });
-    if($('.nav-sidebar-main').children('li.active').length) {
-       var sub = $('.nav-sidebar-main').children('li.active').find('ul.nav-sidebar-children');
-       if(sub.find('li.active').length) {
-           $('.nav-sidebar').addClass('show-sub');
-       }
-    }
-}
-var TestApp = function() {
+};
+
+var App = (function () {
+    "use strict";
+    const _appThemeColors = ["default", "green", "orange"];
+    const _appTheme = _AppTheme.init(_appThemeColors);
+    return {
+        init: function () {
+            this.initSidebar();
+            this.initVendor();
+        },
+        initSidebar: function()  {
+            _handle_sidebar_actions();
+            _handle_sidebar_state();
+        },
+        initVendor: function() {
+            _handleVendor_scrollbot();
+        },
+        theme: _appTheme,
+        helpers: _AppHelpers,
+    };
+})();
+
+var TestApp = (function () {
     "use strict";
     return {
-        init: function() {
-            _handleSidebar();
-        }
+        init: function () {
+            
+        },
     };
-}();
+})();
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(){ 
     App.init();
-	TestApp.init();
-});
+    TestApp.init();
+}, false);
